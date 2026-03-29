6207c621857672fd5e16a32636c74857196dce6f
Olimpos_Bot <olimpos.bot.sinyal@gmail.com>
2026-03-29 14:51:54 +0300

    fix(schema): restore valid schema exports

--- api/schemas/__init__.py
+++ api/schemas/__init__.py
@@ -1,4 +1,15 @@
-# api\schemas\__init__.py
-from .quote import QuoteCreate, QuoteUpdate, QuoteOut, QuoteListOut, MessageOut
+# api/schemas/__init__.py
+from .auth import LogoutRequest, MessageResponse, TokenPairResponse, TokenRefreshRequest
+from .quote import MessageOut, QuoteCreate, QuoteListOut, QuoteOut, QuoteUpdate
 
-__all__ = ["QuoteCreate", "QuoteUpdate", "QuoteOut", "QuoteListOut", "MessageOut"]
+__all__ = [
+    "TokenRefreshRequest",
+    "TokenPairResponse",
+    "LogoutRequest",
+    "MessageResponse",
+    "QuoteCreate",
+    "QuoteUpdate",
+    "QuoteOut",
+    "QuoteListOut",
+    "MessageOut",
+]
